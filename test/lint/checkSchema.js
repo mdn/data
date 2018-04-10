8c994b13a1ac7866cb2f52d9834a41d04174d771
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2018-04-11 00:23:25 +0200

    Fix errors

--- test/lint/checkSchema.js
+++ test/lint/checkSchema.js
@@ -1,19 +1,19 @@
-var fs = require('fs')
-var Ajv = require('ajv')
-var ajv = new Ajv({ $data: true, allErrors: true })
+const fs = require('fs')
+const Ajv = require('ajv')
+const ajv = new Ajv({ $data: true, allErrors: true })
 
-var definitions = require('../../css/definitions.json')
-var properties = require('../../css/properties.json')
+const definitions = require('../../css/definitions.json')
+const properties = require('../../css/properties.json')
 
 ajv.addSchema(definitions, 'definitions')
 ajv.addSchema({ names: { enum: Object.keys(properties) } }, 'properties')
 
 function checkSchema (dataFilename) {
-  var noErrors = true
-  var schemaFilename = dataFilename.replace(/\.json/i, '.schema.json')
+  let noErrors = true
+  const schemaFilename = dataFilename.replace(/\.json/i, '.schema.json')
 
   if (fs.existsSync(schemaFilename)) {
-    var valid = ajv.validate(
+    const valid = ajv.validate(
       require(schemaFilename),
       require(dataFilename)
     )
@@ -22,6 +22,7 @@ function checkSchema (dataFilename) {
       console.log('  JSON Schema – OK')
     } else {
       noErrors = false
+
       console.log('  JSON Schema – ' + ajv.errors.length + ' error(s)\n    ' +
         ajv.errorsText(ajv.errors, {
           separator: '\n    ',
