7d1b578dfe5798b4bacedbb855909b23ba755df8
Dario Aprea <darioxaprea@gmail.com>
2018-07-02 09:20:58 +0200

    Clean up validat-syntaxes test

--- test/validate-syntaxes.js
+++ test/validate-syntaxes.js
@@ -2,9 +2,9 @@ var properties = require('../css/properties');
 var syntaxes = require('../css/syntaxes');
 var basicTypes = require('../css/types');
 var parseGrammar = require('css-tree').grammar.parse;
+var walkGrammar = require('css-tree').grammar.walk;
 
 var hasErrors = false;
-var itemsHaveErrors;
 
 function isBasicType(name) { return basicTypes.hasOwnProperty(name); }
 function isSyntax(name) { return syntaxes.hasOwnProperty(name); }
@@ -13,65 +13,63 @@ function isProperty(name) { return properties.hasOwnProperty(name); }
 // Checks that the syntaxes this grammar refers to exist.
 // Returns an array of errors (strings), one for each broken reference.
 function validateGrammar(grammar) {
-  switch (grammar.type) {
-    case 'Group':
-      // validate the nested grammars and collect all the errors
-      return grammar.terms
-        .map(validateGrammar)
-        .reduce(function(a, b) { return a.concat(b); }, []);
-    case 'Multiplier':
-      // validate the nested grammar
-      return validateGrammar(grammar.term);
-    case 'Type':
-      // validate basic types and non-terminal types, e.g. <length>, <bg-image>
-      var typeName = grammar.name;
-      if (!(isBasicType(typeName) || isSyntax(typeName))) {
-        return ['invalid type: ' + typeName];
-      }
-      return [];
-    case 'Property':
-      // validate references to properties, e.g. <'background-color'>
-      var typeName = grammar.name;
-      if (!isProperty(typeName)) {
-        return ['invalid property: ' + typeName];
-      }
-      return [];
-    default:
-      return [];
-  }
+  var errors = [];
+
+  walkGrammar(grammar, function(node) {
+    switch (node.type) {
+      case 'Type':
+        // validate basic types and non-terminal types, e.g. <length>, <bg-image>
+        var typeName = node.name;
+        if (!isBasicType(typeName) && !isSyntax(typeName)) {
+          errors.push('Broken reference: <' + typeName + '>');
+        }
+        break;
+
+      case 'Property':
+        // validate references to properties, e.g. <'background-color'>
+        var typeName = node.name;
+        if (!isProperty(typeName)) {
+          errors.push('Broken reference: <\'' + typeName + '\'>');
+        }
+        break;
+    }
+  });
+
+  return errors;
 }
 
 function validateItem(name, item) {
   try {
     var syntax = parseGrammar(item.syntax);
-    var errs = validateGrammar(syntax);
-    if (errs.length > 0) {
+    var errors = validateGrammar(syntax);
+    if (errors.length > 0) {
       // print validation errors
       console.log('  ' + name + ':');
-      errs.forEach(function (err) { console.log('    ' + err); });
-      itemsHaveErrors = true;
+      console.log('    ' + errors.join('\n    '));
+      return false;
     }
-  }
-  catch (err) {
+  } catch (err) {
     // print parsing errors
     console.log('  ' + name + ':');
     console.log('    ' + err.message.replace(/\n/g, '\n    '));
-    itemsHaveErrors = true;
+    return false;
   }
 }
 
 function validateItems(file, items) {
   console.log(file);
 
-  itemsHaveErrors = false;
+  var itemsHaveErrors = false;
 
-  Object.keys(items).forEach(function(key) { validateItem(key, items[key]); });
+  for (var key in items) {
+    if (!validateItem(key, items[key])) {
+      itemsHaveErrors = true;
+    }
+  }
 
   if (itemsHaveErrors) {
-    console.log('');
     hasErrors = true;
-  }
-  else {
+  } else {
     console.log('  Syntaxes – OK');
   }
 }
