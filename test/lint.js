f225f8fae19d60f0f5417a8443671fb7fd8f1415
Brian Thomas Smith <brian@smith.berlin>
2023-02-03 11:15:44 +0100

    chore(deps): test bumping ajv to >8.0.0

--- test/lint.js
+++ test/lint.js
@@ -1,29 +1,63 @@
-var fs = require('fs');
-var path = require('path');
-var Ajv = require('ajv');
-var betterAjvErrors = require('better-ajv-errors').default;
-var dictPaths = ['api', 'css', 'l10n'];
+var fs = require("fs");
+var path = require("path");
+var Ajv = require("ajv");
+var betterAjvErrors = require("better-ajv-errors").default;
+var dictPaths = ["api", "css", "l10n"];
 var hasErrors = false;
+
 var ajv = new Ajv({
   $data: true,
   allErrors: true,
-  jsonPointers: true
 });
 
-ajv.addKeyword('property-reference', {
+// See https://ajv.js.org/keywords.html for more information
+
+ajv.addKeyword({
+  keyword: "property-reference",
+  type: "string",
   $data: true,
-  metaSchema: { type: 'object' },
-  validate: function(schema, data, parentSchema) {
+  metaSchema: { type: "object" },
+  validate: function (schema, data) {
     var valid = schema.hasOwnProperty(data);
     if (!valid) {
       // TODO: make a verbose message when invalid
       // throw new Error('wrong reference')
     }
     return valid;
-  }
+  },
 });
 
-ajv.addSchema(require('../css/definitions.json'), 'definitions.json');
+ajv.addKeyword({
+  keyword: "minitems",
+  type: "array",
+  $data: true,
+  metaSchema: { type: "integer" },
+  validate: function (schema, data) {
+    var valid = data.length >= schema;
+    if (!valid) {
+      // TODO: make a verbose message when invalid
+      // throw new Error('wrong reference')
+    }
+    return valid;
+  },
+});
+
+ajv.addKeyword({
+  keyword: "groupList",
+  type: "array",
+  $data: true,
+  metaSchema: { type: "object" },
+  validate: function (schema, data) {
+    var valid = data.length >= schema;
+    if (!valid) {
+      // TODO: make a verbose message when invalid
+      // throw new Error('wrong reference')
+    }
+    return valid;
+  },
+});
+
+ajv.addSchema(require("../css/definitions.json"), "definitions.json");
 
 function jsonDiff(actual, expected) {
   var actualLines = actual.split(/\n/);
@@ -32,28 +66,28 @@ function jsonDiff(actual, expected) {
   for (var i = 0; i < actualLines.length; i++) {
     if (actualLines[i] !== expectedLines[i]) {
       return [
-        '#' + i,
-        '    Actual:   ' + actualLines[i],
-        '    Expected: ' + expectedLines[i]
-      ].join('\n');
+        "#" + i,
+        "    Actual:   " + actualLines[i],
+        "    Expected: " + expectedLines[i],
+      ].join("\n");
     }
   }
 }
 
 function checkStyle(filename) {
-  var actual = fs.readFileSync(filename, 'utf-8').trim();
+  var actual = fs.readFileSync(filename, "utf-8").trim();
   var expected = JSON.stringify(JSON.parse(actual), null, 2);
 
   if (actual === expected) {
-    console.log('  Style – OK');
+    console.log("  Style – OK");
   } else {
     hasErrors = true;
-    console.log('  Style – Error on line ' + jsonDiff(actual, expected));
+    console.log("  Style – Error on line " + jsonDiff(actual, expected));
   }
 }
 
 function checkSchema(dataFilename) {
-  var schemaFilename = dataFilename.replace(/\.json/i, '.schema.json');
+  var schemaFilename = dataFilename.replace(/\.json/i, ".schema.json");
 
   if (fs.existsSync(schemaFilename)) {
     var schema = require(schemaFilename);
@@ -61,10 +95,10 @@ function checkSchema(dataFilename) {
     var valid = ajv.validate(schema, data);
 
     if (valid) {
-      console.log('  JSON Schema – OK');
+      console.log("  JSON Schema – OK");
     } else {
       hasErrors = true;
-      console.log('  JSON Schema – ' + ajv.errors.length + ' error(s)')
+      console.log("  JSON Schema – " + ajv.errors.length + " error(s)");
       // console.log(betterAjvErrors(schema, data, ajv.errors, { indent: 2 }));
 
       // Output messages by one since better-ajv-errors wrongly joins messages
@@ -72,25 +106,25 @@ function checkSchema(dataFilename) {
       // Other issues with better-ajv-errors:
       // - it feels better for performance to output messages one by one rather than a list
       // - it seems to be losing some errors when output a list
-      ajv.errors.forEach(function(error) {
+      ajv.errors.forEach(function (error) {
         var message = betterAjvErrors(schema, data, [error], { indent: 2 });
 
-        console.log('\n    ' + message.replace(/\n/g, '\n    '));
+        console.log("\n    " + message.replace(/\n/g, "\n    "));
       });
     }
   }
 }
 
-dictPaths.forEach(function(dir) {
-  var absDir = path.resolve(path.join(__dirname, '..', dir));
+dictPaths.forEach(function (dir) {
+  var absDir = path.resolve(path.join(__dirname, "..", dir));
 
-  fs.readdirSync(absDir).forEach(function(filename) {
-    if (path.extname(filename) === '.json') {
+  fs.readdirSync(absDir).forEach(function (filename) {
+    if (path.extname(filename) === ".json") {
       var absFilename = path.join(absDir, filename);
 
-      console.log(dir + '/' + filename);
+      console.log(dir + "/" + filename);
 
-      checkStyle(absFilename)
+      checkStyle(absFilename);
       checkSchema(absFilename);
 
       console.log();
